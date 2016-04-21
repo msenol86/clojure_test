b3ffb75bc4db784d8c6906b576f4ecdbd06ad0c5
Mucahit Senol <mucahitsenol86@gmail.com>
2016-04-21 21:36:15 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to my_clojure_proj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/my_clojure_proj/core.clj
@@ -0,0 +1,33 @@
+(ns my-clojure-proj.core
+  (:gen-class))
+
+;;debugging parts of expressions
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
+;; tail-call-optimization
+(defn is_not_prime [x acc1] (cond (= x acc1) false :else (or (= 0 (dbg (mod x acc1))) (recur x (+ acc1 1)))))
+
+
+;; without tail-call-optimization
+(defn is_not_prime_bad [x acc1] (cond (= x acc1) false :else (or (is_not_prime_bad x (+ acc1 1)) (= 0 (dbg (mod x acc1))))))
+
+
+(defn is_prime [x]
+  (cond
+    (= x 1) true
+    (= x 2) true
+    :else (not (is_not_prime x 2))))
+
+(defn is_prime_bad [x]
+  (cond
+    (= x 1) true
+    (= x 2) true
+    :else (not (is_not_prime_bad x 2))))
+
+
+; very big prime number 2147483647
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
