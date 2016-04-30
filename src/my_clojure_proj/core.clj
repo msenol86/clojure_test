dfd79c17896691a53fce80eb5c11a46331f1a0c9
Mucahit Senol <mucahitsenol86@gmail.com>
2016-04-30 15:08:47 +0300

    brave clojure -lazy seqs added

--- src/my_clojure_proj/core.clj
+++ src/my_clojure_proj/core.clj
@@ -100,6 +100,39 @@
 (assert (= '(1 2) (find_weird_numbers_until 5)))
 (assert (= '(1 2 145 40585) (find_weird_numbers_until 40585)))
 
+(defn add1 [x]
+  (+
+    (+ x 2) 1))
+
+
+;; Brave Clojure - Lazy Seqs
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+
+
+(<)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
